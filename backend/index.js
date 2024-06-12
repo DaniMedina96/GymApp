4d69b74736a42fcbb20832ab2421508734b9e481
medina <medina@DESKTOP-28TCG6N>
2024-06-12 20:46:29 +0100

    seguimos pordetras

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,1 @@
-const { Sequelize } = require('sequelize');
-
-const connection = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASS, 
-{
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-});
+const checkDB = require("./database")
